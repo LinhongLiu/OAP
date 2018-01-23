a3154cf604bf201546d522a55957b96513b8c3da
Linhong Liu <linhong.liu@intel.com>
2018-01-23 10:20:47 +0800

    style fix

--- src/test/scala/org/apache/spark/sql/execution/datasources/oap/filecache/FiberCacheManagerSuite.scala
+++ src/test/scala/org/apache/spark/sql/execution/datasources/oap/filecache/FiberCacheManagerSuite.scala
@@ -228,7 +228,7 @@ class FiberCacheManagerSuite extends SharedOapContext {
     val data = generateData(kbSize)
     val fiber = TestFiber(() => MemoryManager.putToDataFiberCache(data), s"get remove test")
     def occupyWork(): Boolean = {
-      (1 to 100000).foreach { _ =>
+      (1 to 100).foreach { _ =>
         val fiberCache = FiberCacheManager.get(fiber, configuration)
         if (fiberCache.isDisposed) {
           fiberCache.release()
