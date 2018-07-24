2f63cc9097189e5366a8008a91872ba5621d9edc
Liu, Linhong <linhong.liu@intel.com>
2018-07-24 14:48:25 +0800

    test for read and write

--- src/test/scala/org/apache/spark/sql/execution/datasources/oap/statistics/SqliteSampleStatisticsSuite.scala
+++ src/test/scala/org/apache/spark/sql/execution/datasources/oap/statistics/SqliteSampleStatisticsSuite.scala
@@ -17,8 +17,14 @@
 
 package org.apache.spark.sql.execution.datasources.oap.statistics
 
+import java.io.ByteArrayOutputStream
+
+import scala.collection.mutable.ArrayBuffer
+import scala.util.Random
+
 import org.apache.hadoop.conf.Configuration
 
+import org.apache.spark.sql.execution.datasources.oap.index.IndexUtils
 import org.apache.spark.sql.types.StructType
 
 class SqliteSampleStatisticsSuite extends StatisticsTest {
@@ -26,22 +32,99 @@ class SqliteSampleStatisticsSuite extends StatisticsTest {
   class TestSqliteSampleWriter(schema: StructType)
     extends SampleStatisticsWriter(schema, new Configuration()) {
 
+    def getSampleArray: Seq[Sample] = sampleArray
   }
 
   class TestSqliteSampleReader(schema: StructType) extends SampleStatisticsReader(schema) {
 
+    def getSampleArray: Seq[Sample] = sampleArray
   }
 
   test("test write function") {
+    val keys = (1 to 300).map(i => rowGen(i)).toArray // keys needs to be sorted
 
+    val testSample = new TestSqliteSampleWriter(schema)
+    testSample.write(out, keys.to[ArrayBuffer])
+
+    var offset = 0
+    val fiber = wrapToFiberCache(out)
+    assert(fiber.getInt(offset) == StatisticsType.TYPE_SQLITE_SAMPLE)
+    offset += 4
+    val rowCount = fiber.getInt(offset)
+    offset += 4
+    val size = fiber.getInt(offset)
+    offset += 4
+    assert(rowCount == keys.length)
+    assert(size == math.max(24, (rowCount * 0.01 + 1).toInt))
+
+    var rowOffset = 0
+    for (i <- 0 until size) {
+      val start = offset + size * 4 + rowOffset
+      val (key, length) = nnkr.readKey(fiber, start)
+      val nEq = fiber.getInt(start + length)
+      val nLt = fiber.getInt(start + length + 4)
+      val nDLt = fiber.getInt(start + length + 4 * 2)
+      rowOffset = fiber.getInt(offset + i * 4)
+      assert(keys.contains(key))
+      assert(nEq == 1)
+      assert(key.getInt(0) == nLt + 1)
+      assert(nLt == nDLt)
+    }
   }
 
   test("read function test") {
+    val keys = (1 to 300).map(i => rowGen(i)).toArray // random order
+    val size = (Random.nextInt() % 200 + 200) % 200 + 10 // assert nonEmpty sample
+    assert(size >= 0 && size <= 300)
 
+    IndexUtils.writeInt(out, StatisticsType.TYPE_SQLITE_SAMPLE)
+    IndexUtils.writeInt(out, keys.length)
+    IndexUtils.writeInt(out, size)
+
+    val tempWriter = new ByteArrayOutputStream()
+    for (idx <- 0 until size) {
+      nnkw.writeKey(tempWriter, keys(idx))
+      IndexUtils.writeInt(tempWriter, 1)
+      IndexUtils.writeInt(tempWriter, keys(idx).getInt(0) - 1)
+      IndexUtils.writeInt(tempWriter, keys(idx).getInt(0) - 1)
+      IndexUtils.writeInt(out, tempWriter.size)
+    }
+    out.write(tempWriter.toByteArray)
+
+    val fiber = wrapToFiberCache(out)
+
+    val testSample = new TestSqliteSampleReader(schema)
+    testSample.read(fiber, 0)
+
+    val array = testSample.getSampleArray
+
+    for (i <- array.indices) {
+      assert(ordering.compare(keys(i), array(i).key) == 0)
+      assert(array(i).nEq == 1)
+      assert(array(i).nLt == keys(i).getInt(0) - 1)
+      assert(array(i).nLt == array(i).nDLt)
+    }
   }
 
   test("read and write") {
+    val keys = (1 to 300).map(i => rowGen(i)).toArray
 
+    val sampleWrite = new TestSqliteSampleWriter(schema)
+    sampleWrite.write(out, keys.to[ArrayBuffer])
+
+    val fiber = wrapToFiberCache(out)
+
+    val sampleRead = new TestSqliteSampleReader(schema)
+    sampleRead.read(fiber, 0)
+
+    val array = sampleRead.getSampleArray
+
+    for (i <- array.indices) {
+      assert(keys.contains(array(i).key))
+      assert(array(i).nEq == 1)
+      assert(array(i).key.getInt(0) == array(i).nLt + 1)
+      assert(array(i).nLt == array(i).nDLt)
+    }
   }
 
   test("test analyze function") {
