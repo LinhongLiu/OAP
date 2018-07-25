ac39dea896b8a97307a64a16046598bd82519676
Liu, Linhong <linhong.liu@intel.com>
2018-07-25 09:25:14 +0800

    style

--- src/test/scala/org/apache/spark/sql/execution/datasources/oap/statistics/SqliteSampleStatisticsSuite.scala
+++ src/test/scala/org/apache/spark/sql/execution/datasources/oap/statistics/SqliteSampleStatisticsSuite.scala
@@ -21,7 +21,9 @@ import java.io.ByteArrayOutputStream
 
 import scala.collection.mutable.ArrayBuffer
 import scala.util.Random
+
 import org.apache.hadoop.conf.Configuration
+
 import org.apache.spark.sql.catalyst.InternalRow
 import org.apache.spark.sql.catalyst.expressions.JoinedRow
 import org.apache.spark.sql.execution.datasources.oap.index.{IndexScanner, IndexUtils}
